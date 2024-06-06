492ad070a6e1259a02879d3875ab0aaadd55c068
Oluwole-young Taiwo <taiwooluwoleyoung@gmail.com>
2024-06-06 23:50:56 +0100

    installed axios

---
+++ github-repo-vue-project/src/components/repos.vue
@@ -0,0 +1,25 @@
+<script>
+    import axios from "axios";
+
+    export default {
+            name: "Repositories",
+            data() {
+                return {
+                    repos: null,
+                };
+            },
+            created: function() {
+                axios.get('https://api.github.com/users/Youngie8/repos').then(response)  => {
+                    this.repos = response.data
+                }
+            }
+    }
+</script>
+
+<template>
+    <h1> Vue</h1>
+</template>
+
+<style>
+
+</style>
